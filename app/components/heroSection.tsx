e0696e0ff81c699c20bade71f77dedb0fefd1c77
Jack DeVries <jdevries3133@gmail.com>
2022-04-24 19:14:48 -0400

    add links for all toolbox items

--- app/components/heroSection.tsx
+++ app/components/heroSection.tsx
@@ -40,7 +40,7 @@ export const HeroSection = () => (
           <h4 className="text-sm sm:text-base">Web & Software Developer</h4>
         </div>
         <div className="w-24 lg:w-48 p-2 bg-primary-100 rounded-full shadow">
-          <img className="rounded-full" src="./static/me@400px.jpg" />
+          <img className="rounded-full" src="/static/me@400px.jpg" />
         </div>
       </div>
     </div>
@@ -75,93 +75,113 @@ export const HeroSection = () => (
             "
         >
           {/* react */}
-          <div data-tooltip="React" className="tooltip">
-            <img alt="react.js icon" src="./static/react.webp" />
-          </div>
+          <a href="https://reactjs.org/">
+            <div data-tooltip="React" className="tooltip">
+              <img alt="react.js icon" src="/static/react.webp" />
+            </div>
+          </a>
           {/* redux */}
-          <div data-tooltip="Redux" className="tooltip">
-            <img
-              className="w-full h-full"
-              alt="redux icon"
-              src="./static/redux.svg"
-            />
-          </div>
-          {/* django */}
-          <div data-tooltip="Django" className="tooltip">
-            <img alt="django icon" src="./static/django.svg" />
-          </div>
-          {/* django REST framework */}
-          <div
-            data-tooltip="Django REST Framework"
-            className="tooltip-drf tooltip"
-          >
-            <img
-              className="rounded-md"
-              alt="django REST framework icon"
-              src="./static/drf.webp"
-            />
-          </div>
+          <a href="https://redux.js.org/">
+            <div data-tooltip="Redux" className="tooltip">
+              <img
+                className="w-full h-full"
+                alt="redux icon"
+                src="/static/redux.svg"
+              />
+            </div>
+          </a>
+          <a href="https://www.djangoproject.com/">
+            {/* django */}
+            <div data-tooltip="Django" className="tooltip">
+              <img alt="django icon" src="/static/django.svg" />
+            </div>
+          </a>
+          <a href="https://www.django-rest-framework.org/">
+            {/* django REST framework */}
+            <div
+              data-tooltip="Django REST Framework"
+              className="tooltip-drf tooltip"
+            >
+              <img
+                className="rounded-md"
+                alt="django REST framework icon"
+                src="/static/drf.webp"
+              />
+            </div>
+          </a>
           {/* tailwind */}
-          <div
-            data-tooltip="Tailwind CSS"
-            className="tooltip-wide tooltip flex items-center col-span-2"
-          >
-            <img
-              alt="Tailwind CSS icon"
-              src="./static/tailwind.svg"
-              className="w-full h-full"
-            />
-          </div>
+          <a href="https://tailwindcss.com/" className="col-span-2">
+            <div
+              data-tooltip="Tailwind CSS"
+              className="tooltip-wide tooltip flex items-center"
+            >
+              <img
+                alt="Tailwind CSS icon"
+                src="/static/tailwind.svg"
+                className="w-full h-full"
+              />
+            </div>
+          </a>
           {/* javascript */}
-          <div data-tooltip="Javascript" className="tooltip">
-            <img
-              className="rounded-md"
-              alt="javascript icon"
-              src="./static/js.webp"
-            />
-          </div>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+            <div data-tooltip="Javascript" className="tooltip">
+              <img
+                className="rounded-md"
+                alt="javascript icon"
+                src="/static/js.webp"
+              />
+            </div>
+          </a>
           {/* git */}
-          <div
-            data-tooltip="Git"
-            className="tooltip p-2 flex items-center bg-clay-100 rounded-md"
-          >
-            <img alt="git icon" src="./static/git.webp" />
-          </div>
+          <a href="https://git-scm.com/">
+            <div
+              data-tooltip="Git"
+              className="tooltip p-2 flex items-center bg-clay-100 rounded-md"
+            >
+              <img alt="git icon" src="/static/git.webp" />
+            </div>
+          </a>
           {/* typescript */}
-          <div data-tooltip="Typescript" className="tooltip-left-0 tooltip">
-            <img
-              className="rounded-md"
-              alt="typescript icon"
-              src="./static/ts.webp"
-            />
-          </div>
-          {/* python */}
-          <div
-            data-tooltip="Python"
-            className="tooltip-wide tooltip col-span-2"
-          >
-            <img alt="python icon" src="./static/python.svg" />
-          </div>
+          <a href="https://www.typescriptlang.org/">
+            <div data-tooltip="Typescript" className="tooltip-left-0 tooltip">
+              <img
+                className="rounded-md"
+                alt="typescript icon"
+                src="/static/ts.webp"
+              />
+            </div>
+          </a>
           {/* PostgreSQL */}
-          <div data-tooltip="PostgreSQL" className="tooltip-left-0 tooltip">
-            <img alt="postgreSQL icon" src="./static/psql.webp" />
-          </div>
+          <a href="https://www.postgresql.org/">
+            <div data-tooltip="PostgreSQL" className="tooltip-left-0 tooltip">
+              <img alt="postgreSQL icon" src="/static/psql.webp" />
+            </div>
+          </a>
+
           {/* python */}
-          <div data-tooltip="Python" className="tooltip-wide tooltip ">
-            <img alt="python icon" src="/static/python.svg" />
-          </div>
+          <a href="https://www.python.org/" className="col-span-2">
+            <div data-tooltip="Python" className="tooltip-wide tooltip ">
+              <img alt="python icon" src="/static/python.svg" />
+            </div>
+          </a>
           {/* Docker */}
-          <div data-tooltip="Docker" className="tooltip-left-0 tooltip">
-            <img alt="docker logo" src="/static/docker.webp" />
-          </div>
+          <a href="https://www.docker.com/">
+            <div data-tooltip="Docker" className="tooltip-left-0 tooltip">
+              <img alt="docker logo" src="/static/docker.webp" />
+            </div>
+          </a>
           {/* Kubernetes */}
-          <div data-tooltip="Kubernetes" className="tooltip-left-0 tooltip">
-            <img alt="kubernetes logo" src="/static/kubernetes.webp" />
-          </div>
+          <a href="https://kubernetes.io/">
+            <div data-tooltip="Kubernetes" className="tooltip-left-0 tooltip">
+              <img alt="kubernetes logo" src="/static/kubernetes.webp" />
+            </div>
+          </a>
           {/* Terraform */}
-          <div data-tooltip="Terraform" className="tooltip">
-            <img alt="terraform logo" src="/static/terraform.webp" />
-          </div>
+          <a href="https://www.terraform.io/">
+            <div data-tooltip="Terraform" className="tooltip">
+              <img alt="terraform logo" src="/static/terraform.webp" />
+            </div>
+          </a>
         </div>
       </div>
     </div>
