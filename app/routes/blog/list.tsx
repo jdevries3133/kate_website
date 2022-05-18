c8cf096da62eba6a3e997123f56e8e25bf0b9b62
Jack DeVries <jdevries3133@gmail.com>
2022-05-18 13:00:16 -0400

    fix client/server date mismatch bug
    * by rendering all dates into strings on the server

--- app/routes/blog/list.tsx
+++ app/routes/blog/list.tsx
@@ -1,6 +1,7 @@
-import { Link, MetaFunction } from "remix";
+import { Link, LoaderFunction, MetaFunction, useLoaderData } from "remix";
 import { useLocales } from "~/providers/localeProvider";
 import { postMetadata } from "~/mdx/postCollections";
+import { parseAcceptLanguage } from "intl-parse-accept-language";
 
 export const meta: MetaFunction = () => {
   return {
@@ -9,18 +10,44 @@ export const meta: MetaFunction = () => {
   };
 };
 
+type LoaderData = {
+  slug: string;
+  title: string;
+  description?: string;
+  created?: string;
+  lastUpdated?: string;
+};
+
+export const loader: LoaderFunction = ({ request }) => {
+  // convert created and lastUpdated (if present) to strings on the server to
+  // avoid client-server mismatches. We don't need to be super precise with the
+  // dates.
+  const locales = parseAcceptLanguage(request.headers.get("accept-language"), {
+    validate: Intl.DateTimeFormat.supportedLocalesOf,
+  });
+  const data: LoaderData[] = postMetadata.map((post) => ({
+    slug: post.slug,
+    title: typeof post.title === "string" ? post.title : "Untitled",
+    description: post.description,
+    created: post?.created?.toLocaleDateString(locales),
+    lastUpdated: post?.lastUpdated?.toLocaleDateString(locales),
+  }));
+  return data;
+};
+
 export default function List() {
-  const locales = useLocales();
-  return postMetadata.map((post) => {
-    const dateStr = post.created.toLocaleDateString(locales);
+  const posts = useLoaderData<LoaderData[]>();
+  return posts.map((post) => {
     return (
       <Link key={post.slug} to={`/blog/${post.slug}`}>
         <div className="max-w-prose p-4 m-2 border-primary bg-clay-200 text-mineral-700 rounded shadow">
           <h3>{post.title}</h3>
-          {dateStr && <p className="text-sm font-light">{dateStr}</p>}
-          <p className="pl-2 border-l-4 border-zinc-200">
-            {post.attributes?.description}
-          </p>
+          {post.created && <p className="text-sm font-light">{post.created}</p>}
+          {post.description && (
+            <p className="pl-2 border-l-4 border-zinc-200">
+              {post.description}
+            </p>
+          )}
         </div>
       </Link>
     );
