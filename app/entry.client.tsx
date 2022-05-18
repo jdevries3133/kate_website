050e195584cab85c92ef9577be0ca730d9f57c74
Jack DeVries <jdevries3133@gmail.com>
2022-05-18 18:55:18 -0400

    remove locale provider; prefer getLocale service

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,13 +1,4 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
-import { LocaleContextProvider } from "./providers/localeProvider";
 
-const locales = window.navigator.languages;
-
-hydrate(
-  <LocaleContextProvider locales={locales as any as string[]}>
-    <RemixBrowser />
-  </LocaleContextProvider>,
-
-  document
-);
+hydrate(<RemixBrowser />, document);
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,8 +1,6 @@
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
-import { parseAcceptLanguage } from "intl-parse-accept-language";
-import { LocaleContextProvider } from "./providers/localeProvider";
 
 export default function handleRequest(
   request: Request,
@@ -10,14 +8,8 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const acceptLanguage = request.headers.get("accept-language");
-  const locales = parseAcceptLanguage(acceptLanguage, {
-    validate: Intl.DateTimeFormat.supportedLocalesOf,
-  });
   const markup = renderToString(
-    <LocaleContextProvider locales={locales}>
-      <RemixServer context={remixContext} url={request.url} />
-    </LocaleContextProvider>
+    <RemixServer context={remixContext} url={request.url} />
   );
 
   responseHeaders.set("Content-Type", "text/html");
