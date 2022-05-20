cb489b4c4b8e1d90219203a33b3874cc7605ad5f
Jack DeVries <jdevries3133@gmail.com>
2022-05-20 14:33:27 -0400

    add delete button to contactform view

--- app/routes/contactMe/results.tsx
+++ app/routes/contactMe/results.tsx
@@ -1,7 +1,22 @@
-import { redirect, LoaderFunction, useLoaderData } from "remix";
+import {
+  Form,
+  redirect,
+  LoaderFunction,
+  useLoaderData,
+  ActionFunction,
+  useTransition,
+} from "remix";
 import { getSession } from "~/sessions";
 import prisma from "~/prisma.server";
 import { ContactInquiry } from "@prisma/client";
+
+export const action: ActionFunction = async ({ request }) => {
+  const id = parseInt(((await request.formData()).get("id") || "") as string);
+  await prisma.contactInquiry.delete({
+    where: { id },
+  });
+  return null;
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
   const session = await getSession(request.headers.get("Cookie"));
@@ -15,6 +30,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Results() {
+  const { state } = useTransition();
   const data = useLoaderData<ContactInquiry[]>();
   return (
     <div className="prose m-4">
@@ -26,15 +42,41 @@ export default function Results() {
             <td>email</td>
             <td>created</td>
             <td>message</td>
+            <td>actions</td>
           </tr>
         </thead>
         <tbody>
-          {data.map(({ name, email, createdAt, message }, i) => (
+          {data.map(({ name, email, createdAt, message, id }, i) => (
             <tr key={i}>
               <td>{name}</td>
               <td>{email}</td>
               <td>{createdAt}</td>
               <td>{message}</td>
+              <td>
+                {["submitting", "loading"].includes(state) ? (
+                  <h3>Loading...</h3>
+                ) : (
+                  <Form method="post">
+                    <input type="hidden" name="id" value={id} />
+                    <button
+                      className="
+                        rounded
+                        shadow
+                        border
+                        p-2
+                        m-2
+                        bg-red-200
+                        hover:bg-red-300
+                        focus:bg-red-300
+                        outline-none
+                        focus:ring-4
+                        focus:ring-red-500"
+                    >
+                      delete
+                    </button>
+                  </Form>
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
