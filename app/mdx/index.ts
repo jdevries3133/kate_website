dd68c2927247c25fde9873c4fcdfe1f4598c3271
Jack DeVries <jdevries3133@gmail.com>
2022-05-27 16:39:43 -0400

    add blog content
    * terraformModules post
    * gitDescribe mini-post

--- app/mdx/index.ts
+++ app/mdx/index.ts
@@ -1,8 +1,10 @@
 import * as myCluster from "./myCluster.mdx";
 import * as clusterApps from "./developingClusterApps.mdx";
+import * as terraformModules from "./terraformModules.mdx";
+import * as gitDescribe from "./gitDescribe.mdx";
 
 /**
  * This is where posts can be published or unpublished. All posts in the array
  * below are published.
  */
-export const allPosts = [myCluster, clusterApps];
+export const allPosts = [terraformModules, gitDescribe, myCluster, clusterApps];
