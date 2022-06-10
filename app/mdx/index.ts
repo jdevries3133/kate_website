684a396aa018996340acbcd95a67788e351b3d84
Jack DeVries <jdevries3133@gmail.com>
2022-06-10 18:43:01 -0400

    constnt: discordUpdate and rustCrossCompile

--- app/mdx/index.ts
+++ app/mdx/index.ts
@@ -2,9 +2,18 @@ import * as myCluster from "./myCluster.mdx";
 import * as clusterApps from "./developingClusterApps.mdx";
 import * as terraformModules from "./terraformModules.mdx";
 import * as gitDescribe from "./gitDescribe.mdx";
+import * as crossCompile from "./rustCrossCompile.mdx";
+import * as updateDiscord from "./discordAutoUpdate.mdx";
 
 /**
  * This is where posts can be published or unpublished. All posts in the array
  * below are published.
  */
-export const allPosts = [terraformModules, gitDescribe, myCluster, clusterApps];
+export const allPosts = [
+  crossCompile,
+  updateDiscord,
+  terraformModules,
+  gitDescribe,
+  myCluster,
+  clusterApps,
+];
