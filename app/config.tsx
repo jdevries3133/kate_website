df44c47af3c0a53f74c23379137296d7ac98b65a
Jack DeVries <jdevries3133@gmail.com>
2022-06-27 16:50:32 -0400

    add debug config, fixup typescript configs

--- app/config.tsx
+++ app/config.tsx
@@ -6,9 +6,8 @@ export const BASE_URL =
 export const HOMEPAGE_LAST_UPDATED = "2022-05-21t08:36:31-04:00";
 
 // in seconds
-export const COOKIE_TIMEOUT = (
-  60 // seconds
-  * 60 // minutes
-  * 24 // hours
-  * 365  // days
-)
+export const COOKIE_TIMEOUT =
+  60 * // seconds
+  60 * // minutes
+  24 * // hours
+  365; // days
--- app/sessions.ts
+++ app/sessions.ts
@@ -1,30 +1,36 @@
 // app/sessions.js
-import { createCookieSessionStorage } from "remix";
+import {
+  CookieSerializeOptions,
+  createCookieSessionStorage,
+  Session,
+} from "remix";
 import { COOKIE_TIMEOUT } from "./config";
 
-const { getSession, commitSession: commitSessionDefaultImpl, destroySession } =
-  createCookieSessionStorage({
-    // a Cookie from `createCookie` or the CookieOptions to create one
-    cookie: {
-      name: "__session",
-      domain:
-        process.env.NODE_ENV === "production" ? "jackdevries.com" : "localhost",
-      httpOnly: true,
-      path: "/",
-      secrets: [process.env.SECRET_KEY || ""],
-      sameSite: "lax",
-      secure: true,
-    },
-  });
+const { getSession, destroySession } = createCookieSessionStorage({
+  // a Cookie from `createCookie` or the CookieOptions to create one
+  cookie: {
+    name: "__session",
+    domain:
+      process.env.NODE_ENV === "production" ? "jackdevries.com" : "localhost",
+    httpOnly: true,
+    path: "/",
+    secrets: [process.env.SECRET_KEY || ""],
+    sameSite: "lax",
+    secure: true,
+  },
+});
 
 /**
  * commitSession wrapper that sets an expiration
  */
-export const commitSession: typeof commitSessionDefaultImpl = (session, opts = {}) => {
+export const commitSession = (
+  session: Session,
+  opts: CookieSerializeOptions = {}
+): Promise<string> => {
   if (opts.expires === undefined) {
     opts.expires = new Date(Date.now() + COOKIE_TIMEOUT);
   }
   return commitSession(session, opts);
-}
+};
 
 export { getSession, destroySession };
