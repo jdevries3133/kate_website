ac9152fd9fc6219b35552184bb4c5fbd476de269
Jack DeVries <jdevries3133@gmail.com>
2022-08-07 21:06:15 -0400

    wip: vim post

--- app/mdx/index.ts
+++ app/mdx/index.ts
@@ -3,12 +3,14 @@ import * as clusterApps from "./developingClusterApps.mdx";
 import * as terraformModules from "./terraformModules.mdx";
 import * as gitDescribe from "./gitDescribe.mdx";
 import * as crossCompile from "./rustCrossCompile.mdx";
+import * as vimTutorial from "./vimTutorial.mdx";
 
 /**
  * This is where posts can be published or unpublished. All posts in the array
  * below are published.
  */
 export const allPosts = [
+  vimTutorial,
   crossCompile,
   terraformModules,
   gitDescribe,
