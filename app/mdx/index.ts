4839a070658d5db305d8531f816aa4f4c43b3549
Jack DeVries <jdevries3133@gmail.com>
2022-08-13 16:51:24 -0400

    vim content

--- app/mdx/index.ts
+++ app/mdx/index.ts
@@ -4,6 +4,9 @@ import * as terraformModules from "./terraformModules.mdx";
 import * as gitDescribe from "./gitDescribe.mdx";
 import * as crossCompile from "./rustCrossCompile.mdx";
 import * as vimTutorial from "./vimTutorial.mdx";
+import * as vimVsNeovim from "./vimVsNeovim.mdx";
+import * as vimMetaConcepts from './vimMetaConcepts.mdx';
+import * as myNeovimConfig from './myNeovimConfig.mdx';
 
 /**
  * This is where posts can be published or unpublished. All posts in the array
@@ -11,6 +14,9 @@ import * as vimTutorial from "./vimTutorial.mdx";
  */
 export const allPosts = [
   vimTutorial,
+  vimMetaConcepts,
+  vimVsNeovim,
+  myNeovimConfig,
   crossCompile,
   terraformModules,
   gitDescribe,
