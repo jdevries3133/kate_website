0b9f1f225fe5c6dfc2439cd898e52dc307cabd26
Kate Tell <katell828@gmail.com>
2022-09-29 22:59:07 -0400

    Revert "publishing content from Thu Sep 29 22:58:57 EDT 2022"
    This reverts commit 29a9f7ad10b5a9980834d3961a66f87e592a8db6.

--- app/mdx/index.ts
+++ app/mdx/index.ts
@@ -1,7 +1,6 @@
 import * as firstPost from "./firstPost.mdx";
-import * as secondPost from "./secondPost.mdx";
 
 /**
  * Posts are published when they are listed here.
  */
-export const allPosts = [secondPost, firstPost];
+export const allPosts = [firstPost];
