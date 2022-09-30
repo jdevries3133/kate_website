29a9f7ad10b5a9980834d3961a66f87e592a8db6
Kate Tell <katell828@gmail.com>
2022-09-29 22:59:03 -0400

    publishing content from Thu Sep 29 22:58:57 EDT 2022

--- app/mdx/index.ts
+++ app/mdx/index.ts
@@ -1,6 +1,7 @@
 import * as firstPost from "./firstPost.mdx";
+import * as secondPost from "./secondPost.mdx";
 
 /**
  * Posts are published when they are listed here.
  */
-export const allPosts = [firstPost];
+export const allPosts = [secondPost, firstPost];
