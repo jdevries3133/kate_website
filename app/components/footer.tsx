678da3e4b403c394fd2203c7118e9be632260877
Jack DeVries <jdevries3133@gmail.com>
2022-10-02 20:35:39 -0400

    fix email

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
   <footer className="justify-center bg-accent-100 rounded-lg p-2 m-2">
     <p className="text-primary-700">
-      Kate Tell · <a href="mailto:katell.lit@gmail.com">Contact Me</a>
+      Kate Tell · <a href="mailto:ktell.lit@gmail.com">Contact Me</a>
     </p>
   </footer>
 );
