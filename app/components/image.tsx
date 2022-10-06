6792858ce0610fdb9352014aa66a08fec32d11e2
Jack DeVries <jdevries3133@gmail.com>
2022-10-05 21:42:56 -0400

    add nnrmo image & harden image component

--- app/components/image.tsx
+++ app/components/image.tsx
@@ -1,11 +1,23 @@
 export const Image: React.FC<{
   src: string;
   alt: string;
-  caption?: string;
+  caption: string;
 }> = ({ src, alt, caption }) => {
+  // this is used in mdx, so we must validate our inputs
+  if (!src || !alt || !caption) {
+    let errStr = '';
+    if (!src) errStr.concat("`src` was not provided to image component");
+    if (!alt) errStr.concat("`alt` was not provided to image component");
+    if (!caption) errStr.concat("`caption` was not provided to image compoonent");
+    throw new Error(errStr);
+  }
   return (
     <div className="not-prose">
-      <img className="w-full" src={src} alt={alt} />
+      <img
+        className="w-full"
+        src={src}
+        alt={alt || caption || "undescribed image"}
+      />
       <span className="text-secondary-800 text-sm">{caption}</span>
     </div>
   );
